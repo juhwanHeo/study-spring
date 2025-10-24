3234934840cceab3d871e42c62b8656da7a6d461
hapyjin <wimver3442@gmail.com>
2025-10-24 14:40:04 +0900

    fix: main log

--- spring-fw-hyunjin/src/main/java/com/fw/Main.java
+++ spring-fw-hyunjin/src/main/java/com/fw/Main.java
@@ -6,10 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
   public static void main(String[] args) {
-
-    log.info("spring framework !!");
-    for (int i = 1; i <= 5; i++) {
-      log.info("i: {}", i);
-    }
+    log.info("Hello world");
   }
 }
