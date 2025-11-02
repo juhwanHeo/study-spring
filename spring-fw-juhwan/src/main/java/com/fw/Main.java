9b27c45fd4959f194e4b859c0a9e9251f3b919b8
juhwanHeo <juhwanheo.dev@gmail.com>
2025-11-02 14:03:49 +0900

    feat: 자신의 빈 load해서 출력

--- spring-fw-juhwan/src/main/java/com/fw/Main.java
+++ spring-fw-juhwan/src/main/java/com/fw/Main.java
@@ -1,15 +1,17 @@
 package com.fw;
 
+import com.fw.week2.Person;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("week2.xml");
+    Person helloBean = context.getBean("juhwan", Person.class);
 
-    log.info("spring framework !!");
-    for (int i = 1; i <= 5; i++) {
-      log.info("i: {}", i);
-    }
+    helloBean.hello();
   }
 }
