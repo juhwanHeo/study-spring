b885c5d4b487ff9a3a06080311e2fe7b8ef97b9b
juwonlee-dev <juwonlee.dev@gmail.com>
2025-11-15 22:56:37 +0900

    feat: add Person class feat: get Bean Obj using ApplicationContext

---
+++ spring-fw-juwonlee/src/main/java/com/fw/week2/Person.java
@@ -0,0 +1,19 @@
+package com.fw.week2;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    private String birthDate;
+
+    public void hello() {
+        log.info("Person info: {}", this);
+    }
+}
