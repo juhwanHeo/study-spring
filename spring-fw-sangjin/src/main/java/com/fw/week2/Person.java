e2f1cc61552f621f26bbf7228453ea8767ccb5dc
sangjin <sangjin0521@naver.com>
2025-11-15 15:34:13 +0900

    Week2 / DI / IoC / Sangjin

---
+++ spring-fw-sangjin/src/main/java/com/fw/week2/Person.java
@@ -0,0 +1,48 @@
+package com.fw.week2;
+
+public class Person {
+    private String name;      // 이름
+    private int age;          // 나이
+    private String gender;    // 성별 (F / M)
+    private String birthDate; // 생일 (YYYY-MM-DD)
+
+    public Person() {}
+
+    public Person(String name, int age, String gender, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.birthDate = birthDate;
+    }
+
+    // getters / setters (JavaBean 규약)
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getBirthDate() { return birthDate; }
+    public void setBirthDate(String birthDate) { this.birthDate = birthDate; }
+
+    // 소개 메서드
+    public void hello() {
+        System.out.println(
+                "안녕하세요, 저는 " + name + "입니다. 나이 " + age +
+                        ", 성별 " + gender + ", 생일 " + birthDate + " 입니다."
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                '}';
+    }
+}
