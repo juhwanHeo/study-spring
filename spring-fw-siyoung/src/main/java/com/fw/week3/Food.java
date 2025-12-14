3a40517a4fe743d9cc4c332d856af52769ea51a2
Siyoung Woo <dntud55@gmail.com>
2025-12-14 18:57:48 +0900

    Week3 / depends-on, Lazy / Siyoung

---
+++ spring-fw-siyoung/src/main/java/com/fw/week3/Food.java
@@ -0,0 +1,26 @@
+package com.fw.week3;
+
+public class Food {
+  String name;
+  String type;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void introduce() {
+    System.out.printf("음식: %s / 종류 : %s %n", name, type);
+  }
+}
