e05c68d14d3f9e104e9d5d4ab8c8d1f2a57ccf60
juhwanHeo <juhwanheo.dev@gmail.com>
2026-01-01 18:22:35 +0900

    feat(Task 2): Food 클래스 추가

---
+++ spring-fw-juhwan/src/main/java/com/fw/week3/Food.java
@@ -0,0 +1,35 @@
+package com.fw.week3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+@AllArgsConstructor
+public class Food {
+
+  private String name;
+  private Type type;
+
+  public Food() {
+    log.info("Food 생성!");
+  }
+
+  @Getter
+  public enum Type {
+    KOR("한식"),
+    CHN("중식"),
+    JPN("일식"),
+    WESTERN("양식"),
+    ETC("기타"),
+    ;
+
+    private final String value;
+
+    Type(String value) {
+      this.value = value;
+    }
+  }
+}
