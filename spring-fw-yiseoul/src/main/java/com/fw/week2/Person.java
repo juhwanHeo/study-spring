b01adf0ba5b5ff8548c767a82ff7f43880f1ae59
LSHgy <his5423@gmail.com>
2026-01-18 01:27:01 +0900

    feat(task3): Food, Person 기본 생성자 생성 로그 추가

--- spring-fw-yiseoul/src/main/java/com/fw/week2/Person.java
+++ spring-fw-yiseoul/src/main/java/com/fw/week2/Person.java
@@ -1,6 +1,7 @@
 package com.fw.week2;
 
 import com.fw.week3.Food;
+import com.fw.week3.Vegetable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Data
-@NoArgsConstructor
 public class Person {
   private String name;
   private int age;
   private String gender;
   private String birthDate;
   private Food food;
+
+  Person() {
+    log.info("Person 생성!");
+  }
 
   public void hello() {
     log.info("이름은 {} 나이 {}의 성별 {}인 사람. 생일은 {}입니다.", name,  age, gender, birthDate);
