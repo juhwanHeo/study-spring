7f49973889cf48d2c70d8f06ed6552f709e52983
LSHgy <his5423@gmail.com>
2026-01-18 00:26:32 +0900

    feat(task2): Person 클래스에 Food 필드 추가 및 관련 로직 수정
    - Food 객체 주입을 위한 필드 추가 및 출력 함수(hello) 반영
    - 불필요한 생성자 제거(@AllArgsConstructor)

--- spring-fw-yiseoul/src/main/java/com/fw/week2/Person.java
+++ spring-fw-yiseoul/src/main/java/com/fw/week2/Person.java
@@ -1,5 +1,6 @@
 package com.fw.week2;
 
+import com.fw.week3.Food;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,14 +9,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Person {
   private String name;
   private int age;
   private String gender;
   private String birthDate;
+  private Food food;
 
   public void hello() {
     log.info("이름은 {} 나이 {}의 성별 {}인 사람. 생일은 {}입니다.", name,  age, gender, birthDate);
+    log.info("좋아하는 음식은 {} {}입니다.", food.getType(),  food.getName());
   }
 }
