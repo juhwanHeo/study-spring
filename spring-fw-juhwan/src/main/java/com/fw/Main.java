556be4b38ef1c086a07884eb8debc79c1ec419bb
juhwanHeo <juhwanheo.dev@gmail.com>
2026-02-01 01:18:28 +0900

    feat(task1): Task1 Main 수정

--- spring-fw-juhwan/src/main/java/com/fw/Main.java
+++ spring-fw-juhwan/src/main/java/com/fw/Main.java
@@ -2,16 +2,18 @@ package com.fw;
 
 import com.fw.week2.Person;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    ApplicationContext context = new ClassPathXmlApplicationContext("week3.xml");
+    ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("week4.xml");
     Person person = context.getBean("juhwan", Person.class);
 
     person.hello();
+
+    context.close();
   }
 }
