161ed3e7ec95f8634eed71701ea1343b727c55f1
Siyoung Woo <dntud55@gmail.com>
2026-02-18 21:38:05 +0900

    Week4 / Lifecycle, Bean Inheritance, BeanPostProcessor / Siyoung

--- spring-fw-siyoung/src/main/java/com/fw/week3/Food.java
+++ spring-fw-siyoung/src/main/java/com/fw/week3/Food.java
@@ -1,5 +1,8 @@
 package com.fw.week3;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class Food {
   String name;
   String type;
@@ -18,5 +21,15 @@ public class Food {
 
   public String getType() {
     return type;
+  }
+
+  @PostConstruct
+  public void init() {
+    System.out.println("Food Init");
+  }
+
+  @PreDestroy
+  public void shutdown() {
+    System.out.println("Food Shutdown");
   }
 }
