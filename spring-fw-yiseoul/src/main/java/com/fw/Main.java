07b6b240f1065ef16177296704612328e34d3280
LSHgy <his5423@gmail.com>
2026-02-18 23:32:02 +0900

    build(task1): annotaion-api 의존성 추가 및 wee4.xml 파일추가 - Main에 빈 설정파일 week4.xml로 지정 - 빈 종료 훅을 위해 ApplicationContext에서 ConfigurableApplicationContext로 변경

--- spring-fw-yiseoul/src/main/java/com/fw/Main.java
+++ spring-fw-yiseoul/src/main/java/com/fw/Main.java
@@ -3,18 +3,19 @@ package com.fw;
 import com.fw.week2.Person;
 import com.fw.week3.Vegetable;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    log.info("Hello World!!"); // 1주차
-    ApplicationContext context = new ClassPathXmlApplicationContext("week3.xml");
+    ConfigurableApplicationContext context = new GenericXmlApplicationContext("week4.xml");
     Person person = (Person) context.getBean("yiseul");
     Vegetable vegetable = (Vegetable) context.getBean("vegetable");
     person.hello();
     vegetable.print();
+
+    context.registerShutdownHook();
   }
 }
