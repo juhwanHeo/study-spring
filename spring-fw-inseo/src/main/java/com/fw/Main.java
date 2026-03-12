dc855d2967b29eea6c758ba0a385b005cf69d265
inseokim1 <dlstj2032@gmail.com>
2026-03-13 05:54:40 +0900

    feat: week2 person bean 구현

--- spring-fw-inseo/src/main/java/com/fw/Main.java
+++ spring-fw-inseo/src/main/java/com/fw/Main.java
@@ -1,6 +1,18 @@
 package com.fw;
+
+import com.fw.week2.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("week2.xml");
+
+        Person person = (Person) context.getBean("inseo");
+
+        person.hello();
     }
 }
