ddc858e39173f8e517f4e6eeaa76d65edcb7d9c1
inseokim1 <dlstj2032@gmail.com>
2026-03-13 08:45:46 +0900

    week2/inseo

--- spring-fw-inseo/src/main/java/com/fw/Main.java
+++ spring-fw-inseo/src/main/java/com/fw/Main.java
@@ -1,18 +1,6 @@
 package com.fw;
-
-import com.fw.week2.Person;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class Main {
-
     public static void main(String[] args) {
-
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("week2.xml");
-
-        Person person = (Person) context.getBean("inseo");
-
-        person.hello();
+        System.out.println("Hello World");
     }
 }
