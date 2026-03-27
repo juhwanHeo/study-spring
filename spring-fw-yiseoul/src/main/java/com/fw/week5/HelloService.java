12f45218cadf242816d2f639fea6eddfc64bb8a1
LSHgy <his5423@gmail.com>
2026-03-27 13:30:08 +0900

    feat(task3): 구현 클래스 의존을 인터페이스로 수정
    - HelloService의 구현체 필드 타입을 인터페이스 타입으로 변경
    - @Qualifier로 구현체 선택

--- spring-fw-yiseoul/src/main/java/com/fw/week5/HelloService.java
+++ spring-fw-yiseoul/src/main/java/com/fw/week5/HelloService.java
@@ -2,17 +2,20 @@ package com.fw.week5;
 
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class HelloService {
 
   @Autowired
-  private MyServiceImpl myService;
+  @Qualifier("myService")
+  private MyService myServiceImpl;
 
   @Resource
-  private GamjaServiceImpl gamjaService;
+  @Qualifier("gamjaService")
+  private MyService gamjaServiceImpl;
 
   public void sayHello() {
-    myService.hello();
-    gamjaService.hello();
+    myServiceImpl.hello();
+    gamjaServiceImpl.hello();
   }
 }
