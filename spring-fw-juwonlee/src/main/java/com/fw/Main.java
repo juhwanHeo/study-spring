9f826132fcd16164f66e29fd6ec8591b95e6383b
juwonlee-dev <juwonlee.dev@gmail.com>
2026-03-28 19:41:39 +0900

    refactor: Remove unused import in Main

--- spring-fw-juwonlee/src/main/java/com/fw/Main.java
+++ spring-fw-juwonlee/src/main/java/com/fw/Main.java
@@ -1,11 +1,9 @@
 package com.fw;
 
-import com.fw.week5.GamjaServiceImpl;
 import com.fw.week5.HelloService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.fw.week5.MyServiceImpl;
 
 @Slf4j
 public class Main {
