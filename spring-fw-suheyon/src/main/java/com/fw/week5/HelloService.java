6b807c88ffef7af7dbb2c0ecd14cd15582420197
zzik_sh <happytngus1@gmail.com>
2026-03-28 21:01:31 +0900

    feat(Task 3): @Qualifier 실습

--- spring-fw-suheyon/src/main/java/com/fw/week5/HelloService.java
+++ spring-fw-suheyon/src/main/java/com/fw/week5/HelloService.java
@@ -3,15 +3,18 @@ package com.fw.week5;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 @Slf4j
 public class HelloService {
 
   @Autowired
-  private MyServiceImpl myServiceImpl;
+  @Qualifier("myServiceImpl")
+  private MyService myServiceImpl;
 
   @Resource
-  private GamjaServiceImpl gamjaServiceImpl;
+  @Qualifier("gamjaServiceImpl")
+  private MyService gamjaServiceImpl;
 
   public void sayHello() {
     log.info("[HelloService] sayHello");
