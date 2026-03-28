bc3114c4fab14d8d9f098ac7bb79dcf3409ddcf8
juwonlee-dev <juwonlee.dev@gmail.com>
2026-03-28 19:40:57 +0900

    refactor: Add Qualifier annotations for service injection in HelloService

--- spring-fw-juwonlee/src/main/java/com/fw/week5/HelloService.java
+++ spring-fw-juwonlee/src/main/java/com/fw/week5/HelloService.java
@@ -2,14 +2,17 @@ package com.fw.week5;
 
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class HelloService {
 
     @Autowired
-    private MyServiceImpl myService;
+    @Qualifier("myServiceImpl")
+    private MyService myService;
 
     @Resource
-    private GamjaServiceImpl gamjaService;
+    @Qualifier("gamjaServiceImpl")
+    private MyService gamjaService;
 
     public void sayHello() {
         myService.hello();
