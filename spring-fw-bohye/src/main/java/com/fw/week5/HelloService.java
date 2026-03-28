aba9150e1789104b1da7cb5dfa89960a959501df
bobo-h <166573095+bobo-h@users.noreply.github.com>
2026-03-29 00:46:32 +0900

    refactor: MyService 인터페이스 의존으로 변경하고 @Qualifier 주입 적용

--- spring-fw-bohye/src/main/java/com/fw/week5/HelloService.java
+++ spring-fw-bohye/src/main/java/com/fw/week5/HelloService.java
@@ -1,15 +1,17 @@
 package com.fw.week5;
 
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class HelloService {
 
     @Autowired
-    private MyServiceImpl myServiceImpl;
+    @Qualifier("myServiceImpl")
+    private MyService myServiceImpl;
 
-    @Resource
-    private GamjaServiceImpl gamjaServiceImpl;
+    @Autowired
+    @Qualifier("gamjaServiceImpl")
+    private MyService gamjaServiceImpl;
 
     public void sayHello() {
         myServiceImpl.hello();
