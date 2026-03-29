0d9501c0f1ac05f84873db9cc165614f0e368a16
inseokim1 <dlstj2032@gmail.com>
2026-03-29 17:18:21 +0900

    줄바꿈 삭제

--- spring-fw-inseo/src/main/java/com/fw/Main.java
+++ spring-fw-inseo/src/main/java/com/fw/Main.java
@@ -5,16 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("week3.xml");
-
         // 1. Person 먼저 호출
         Person person = context.getBean("inseo", Person.class);
         person.hello();
-
         System.out.println("-----");
-
         // 2. Vegetable 나중에 호출
         Vegetable veg = context.getBean("vegetable", Vegetable.class);
         veg.print();
--- spring-fw-inseo/src/main/java/com/fw/week2/Person.java
+++ spring-fw-inseo/src/main/java/com/fw/week2/Person.java
@@ -3,22 +3,16 @@ import com.fw.week3.Food;
 import com.fw.week3.Vegetable;
 
 public class Person {
-
     private String name;
     private int age;
     private GenderType gender;
     private String birthDate;
-
     // 추가
     private Food food;
-
-
     public Person() {
         System.out.println("Person 생성!");
     }
-
     // 기존 Getter / Setter 유지
-
     public String getName() {
         return name;
     }
@@ -26,7 +20,6 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
@@ -34,7 +27,6 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
-
     public GenderType getGender() {
         return gender;
     }
@@ -42,22 +34,16 @@ public class Person {
     public void setGender(GenderType gender) {
         this.gender = gender;
     }
-
     public String getBirthDate() {
         return birthDate;
     }
-
     public void setBirthDate(String birthDate) {
         this.birthDate = birthDate;
     }
-
-    // ✅ Food setter 추가
+    //  Food setter 추가
     public void setFood(Food food) {
         this.food = food;
     }
-
-
-
     public enum GenderType {
         M, F
     }
@@ -67,11 +53,9 @@ public class Person {
         System.out.println("age: " + age);
         System.out.println("gender: " + gender);
         System.out.println("birth date: " + birthDate);
-
         //  추가 출력
         if (food != null) {
             food.print();
         }
-
     }
 }
