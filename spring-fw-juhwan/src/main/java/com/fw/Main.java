9860844c7cd018df687df353170bed25249e4221
juhwanHeo <juhwanheo.dev@gmail.com>
2026-04-11 14:30:19 +0900

    feat(Task3): gamjaServiceImpl 빈 호출 시 새로 인스턴스 생성하도록 수정

--- spring-fw-juhwan/src/main/java/com/fw/Main.java
+++ spring-fw-juhwan/src/main/java/com/fw/Main.java
@@ -15,5 +15,9 @@ public class Main {
 
     HelloService helloService = context.getBean(HelloService.class);
     helloService.sayHello();
+
+    for (int i = 0; i < 10; i++) {
+      context.getBean("gamjaServiceImpl");
+    }
   }
 }
