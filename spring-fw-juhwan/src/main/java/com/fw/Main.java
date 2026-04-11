b2e4670edb287b6258a6860351b5029314a3f5b0
juhwanHeo <juhwanheo.dev@gmail.com>
2026-04-11 14:23:29 +0900

    feat(Task2): Annotation based 빈 주입 사전 작업

--- spring-fw-juhwan/src/main/java/com/fw/Main.java
+++ spring-fw-juhwan/src/main/java/com/fw/Main.java
@@ -1,18 +1,19 @@
 package com.fw;
 
 import com.fw.week5.HelloService;
+import com.fw.week6.AppConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("week5.xml");
-    HelloService helloService = context.getBean(HelloService.class);
+    ApplicationContext context =
+        new AnnotationConfigApplicationContext(AppConfig.class);
 
+    HelloService helloService = context.getBean(HelloService.class);
     helloService.sayHello();
-    context.close();
   }
 }
