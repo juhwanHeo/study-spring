2e84e9056999cb32039cde9e7b16a51b863c32c0
juhwanHeo <juhwanheo.dev@gmail.com>
2026-04-11 14:24:01 +0900

    feat(Task2): Annotation based `@Service` 정의

--- spring-fw-juhwan/src/main/java/com/fw/week5/MyServiceImpl.java
+++ spring-fw-juhwan/src/main/java/com/fw/week5/MyServiceImpl.java
@@ -1,8 +1,10 @@
 package com.fw.week5;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service("myServiceImpl")
 public class MyServiceImpl implements MyService {
 
   @Override
