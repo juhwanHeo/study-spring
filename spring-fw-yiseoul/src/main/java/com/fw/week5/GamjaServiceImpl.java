2499405147c5a76d85a7eea04e7039a1e1b8538d
LSHgy <his5423@gmail.com>
2026-04-26 01:11:38 +0900

    feat(task2):@Service to ServiceImpl
    - Apply @Service stereotype annotation to MyServiceImpl and GamjaServiceImpl, HelloService

--- spring-fw-yiseoul/src/main/java/com/fw/week5/GamjaServiceImpl.java
+++ spring-fw-yiseoul/src/main/java/com/fw/week5/GamjaServiceImpl.java
@@ -3,8 +3,10 @@ package com.fw.week5;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service("gamjaService")
 public class GamjaServiceImpl implements MyService{
 
   @Value("${gamja.count}")
