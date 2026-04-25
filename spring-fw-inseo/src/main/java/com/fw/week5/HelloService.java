8d93ee46888785873978cb50ed059ebbe12456ba
inseokim1 <dlstj2032@gmail.com>
2026-04-25 22:14:09 +0900

    @Qualifier 실습

--- spring-fw-inseo/src/main/java/com/fw/week5/HelloService.java
+++ spring-fw-inseo/src/main/java/com/fw/week5/HelloService.java
@@ -1,18 +1,20 @@
 package com.fw.week5;
 
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class HelloService {
 
     @Autowired
-    private MyServiceImpl myServiceImpl;
+    @Qualifier("myServiceImpl")
+    private MyService myService;
 
-    @Resource
-    private GamjaServiceImpl gamjaServiceImpl;
+    @Autowired
+    @Qualifier("gamjaServiceImpl")
+    private MyService gamjaService;
 
     public void sayHello() {
-        myServiceImpl.hello();
-        gamjaServiceImpl.hello();
+        myService.hello();
+        gamjaService.hello();
     }
 }
