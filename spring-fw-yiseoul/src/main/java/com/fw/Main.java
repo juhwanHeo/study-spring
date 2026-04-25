2067a350fcbb5e0bf946f4f92ca473e3f199bf9e
LSHgy <his5423@gmail.com>
2026-04-26 01:09:30 +0900

    feat(task2):ComponentScan, PropertySource 설정, Main.java context 로드

--- spring-fw-yiseoul/src/main/java/com/fw/Main.java
+++ spring-fw-yiseoul/src/main/java/com/fw/Main.java
@@ -1,21 +1,17 @@
 package com.fw;
 
-import com.fw.week2.Person;
-import com.fw.week3.Vegetable;
 import com.fw.week5.HelloService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    ConfigurableApplicationContext context = new GenericXmlApplicationContext("week5.xml");
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
     HelloService helloService = (HelloService) context.getBean("helloService");
     helloService.sayHello();
-
-    context.close();
   }
 }
