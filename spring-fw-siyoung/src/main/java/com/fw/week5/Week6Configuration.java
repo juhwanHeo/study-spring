77c69e068b9ed1b3077e93dd1885fa3bd1e42444
Siyoung Woo <dntud55@gmail.com>
2026-04-26 14:50:23 +0900

    Week6 / Siyoung

---
+++ spring-fw-siyoung/src/main/java/com/fw/week5/Week6Configuration.java
@@ -0,0 +1,12 @@
+package com.fw.week5;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("com.fw.week5")
+@PropertySource("classpath:gamja.properties")
+public class Week6Configuration {
+
+}
