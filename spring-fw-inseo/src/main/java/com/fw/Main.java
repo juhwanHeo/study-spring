da430dfac5b5019f7a7f5c28869ebcafe90e2224
inseokim1 <dlstj2032@gmail.com>
2026-04-26 16:38:32 +0900

    3. GamjaServiceImpl 빈을 사용할 때마다 새로운 빈을 만들어 사용하도록 Scope를 지정

--- spring-fw-inseo/src/main/java/com/fw/Main.java
+++ spring-fw-inseo/src/main/java/com/fw/Main.java
@@ -1,6 +1,7 @@
 package com.fw;
 
 import com.fw.week5.HelloService;
+import com.fw.week5.MyService;
 import com.fw.week6.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,6 +13,15 @@ public class Main {
 
         HelloService helloService = context.getBean(HelloService.class);
         helloService.sayHello();
+
+        System.out.println("----- GamjaServiceImpl prototype scope 확인 -----");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "번째 GamjaServiceImpl 호출");
+            MyService gamjaService =
+                    context.getBean("gamjaServiceImpl", MyService.class);
+            gamjaService.hello();
+        }
 
         context.close();
     }
--- spring-fw-inseo/src/main/java/com/fw/week5/GamjaServiceImpl.java
+++ spring-fw-inseo/src/main/java/com/fw/week5/GamjaServiceImpl.java
@@ -1,17 +1,22 @@
 package com.fw.week5;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service("gamjaServiceImpl")
+@Scope("prototype")
 public class GamjaServiceImpl implements MyService {
 
     @Value("${gamja.count}")
     private int count;
 
+    public GamjaServiceImpl() {
+        System.out.println("GamjaServiceImpl 새 빈 생성");
+    }
+
     @Override
     public void hello() {
-        System.out.println("GamjaServiceImpl Hello");
         System.out.println("GamjaServiceImpl gamja count: " + count);
     }
 }
