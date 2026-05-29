94e5b6bc36a6f8f9b3a6f9241c6acb0b123e0281
zzik_sh <happytngus1@gmail.com>
2026-05-30 00:44:57 +0900

    feat(Task 2): @Configuration 사용해보기

---
+++ spring-fw-suheyon/src/main/java/com/fw/week7/Gamja.java
@@ -0,0 +1,13 @@
+package com.fw.week7;
+
+public class Gamja {
+  private int count;
+
+  public Gamja(int count) {
+    this.count = count;
+  }
+
+  public int getCount() {
+    return count;
+  }
+}
