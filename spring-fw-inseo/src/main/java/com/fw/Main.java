6008496b1144fb5dd8295d38d1b3abea6a8b9837
inseokim1 <dlstj2032@gmail.com>
2026-05-31 17:20:10 +0900

    feat : use ObjectProvider for GamjaService 1번 문제

--- spring-fw-inseo/src/main/java/com/fw/Main.java
+++ spring-fw-inseo/src/main/java/com/fw/Main.java
@@ -1,7 +1,6 @@
 package com.fw;
 
 import com.fw.week5.HelloService;
-import com.fw.week5.MyService;
 import com.fw.week6.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,15 +12,6 @@ public class Main {
 
         HelloService helloService = context.getBean(HelloService.class);
         helloService.sayHello();
-
-        System.out.println("----- GamjaServiceImpl prototype scope 확인 -----");
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i + "번째 GamjaServiceImpl 호출");
-            MyService gamjaService =
-                    context.getBean("gamjaServiceImpl", MyService.class);
-            gamjaService.hello();
-        }
 
         context.close();
     }
