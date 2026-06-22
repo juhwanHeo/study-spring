521f671b326562eeedee99021b783a918d5d5163
hapyjin <wimver3442@gmail.com>
2026-06-23 00:09:53 +0900

    feat(Task2) : MyServiceImpl 클래스 생성

--- spring-fw-hyunjin/src/main/java/com/fw/Main.java
+++ spring-fw-hyunjin/src/main/java/com/fw/Main.java
@@ -1,22 +1,17 @@
 package com.fw;
 
-import com.fw.week2.Person;
-import com.fw.week3.Vegetable;
+import com.fw.week5.MyServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-@Slf4j
 public class Main {
 
-  public static void main(String[] args) {
-      ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("week4.xml");
-      Person person = context.getBean("hyunjin",Person.class);
-      Vegetable vegetable = context.getBean("vegetable",Vegetable.class);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("week5.xml");
 
-      person.hello();
-      vegetable.print();
+        MyServiceImpl myService = context.getBean("myServiceImpl", MyServiceImpl.class);
+        myService.hello();
 
-      context.close();
-  }
+        context.close();
+    }
 }
