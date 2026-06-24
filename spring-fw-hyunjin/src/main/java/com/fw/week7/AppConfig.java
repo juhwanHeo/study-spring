dd16b34611ac403d0acb51a808c0fff216ef9b8d
hapyjin <wimver3442@gmail.com>
2026-06-24 23:58:05 +0900

    feat(Task2)

---
+++ spring-fw-hyunjin/src/main/java/com/fw/week7/AppConfig.java
@@ -0,0 +1,27 @@
+package com.fw.week7;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+@ComponentScan("com.fw")
+@PropertySource("classpath:gamja.properties")
+public class AppConfig {
+
+    @Value("${gamja.count}")
+    private int count;
+
+    @Bean
+    public Gamja gamja() {
+        return new Gamja(count);
+    }
+
+    @Bean
+    public Transfer transfer(Gamja gamja) {
+        return new Transfer(gamja);
+    }
+}
