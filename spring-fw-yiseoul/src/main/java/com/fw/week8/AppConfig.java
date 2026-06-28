c248c92131bca00f1e173dd47bc9d60951867551
LSHgy <his5423@gmail.com>
2026-06-28 23:30:44 +0900

    feat(task3): 프로파일별 properties 로드 추가
    - 기존 gamja.properties 프로퍼티소스 제거 (프로파일별 properties로 대체)

--- spring-fw-yiseoul/src/main/java/com/fw/week8/AppConfig.java
+++ spring-fw-yiseoul/src/main/java/com/fw/week8/AppConfig.java
@@ -4,15 +4,24 @@ import com.fw.week7.Gamja;
 import com.fw.week7.Transfer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:gamja.properties")
 @Import(TransferRegistrar.class)
 public class AppConfig {
+
+  @Configuration
+  @Profile("dev")
+  @PropertySource("classpath:application-dev.properties")
+  static class DevConfig {}
+
+  @Configuration
+  @Profile("prod")
+  @PropertySource("classpath:application-prod.properties")
+  static class ProdConfig {}
 
   @Value("${gamja.count}")
   private int count;
