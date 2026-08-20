3123a6c17519da9079fc66a146841b33c703c4ce
juhwanHeo <juhwanheo.dev@gmail.com>
2026-08-21 00:06:11 +0900

    refactor(Task1): 9주차 메인 수정

--- spring-fw-juhwan/src/main/java/com/fw/Main.java
+++ spring-fw-juhwan/src/main/java/com/fw/Main.java
@@ -1,24 +1,17 @@
 package com.fw;
 
-import com.fw.week8.AppConfig;
-import com.fw.week7.Transfer;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
+import com.fw.week9.AppConfig;
+import com.fw.week9.OrderEvent;
+import com.fw.week9.OrderPublisher;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.Environment;
 
-@Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    ApplicationContext context =
-        new AnnotationConfigApplicationContext(AppConfig.class);
-//    Transfer transfer = context.getBean(Transfer.class);
-//    transfer.transfer();
-
-    Environment env = context.getEnvironment();
-    log.info("{} 환경입니다.", env.getActiveProfiles()[0]);
-    log.info("gamja {} 개", env.getProperty("gamja.count", "0"));
-
+    try (AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(AppConfig.class)) {
+      OrderPublisher publisher = context.getBean(OrderPublisher.class);
+      publisher.publish(new OrderEvent(1L, "감자", 3));
+    }
   }
 }
