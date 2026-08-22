b77efcaa3700fd22175d89780c8488a8437744b3
hapyjin <wimver3442@gmail.com>
2026-08-22 12:46:17 +0900

    feat(Task3): 환경별 프로퍼티 파일 및 Main 환경 출력 로직 추가

--- spring-fw-hyunjin/src/main/java/com/fw/Main.java
+++ spring-fw-hyunjin/src/main/java/com/fw/Main.java
@@ -1,20 +1,27 @@
 package com.fw;
 
 import com.fw.week7.Transfer;
-import com.fw.week7.AppConfig;
+import com.fw.week8.AppConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
 
 @Slf4j
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "prod");
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Transfer transfer = context.getBean(Transfer.class);
-        transfer.transfer();
+//        Transfer transfer = context.getBean(Transfer.class);
+//        transfer.transfer();
+
+        Environment env = context.getEnvironment();
+        log.info("{} 환경입니다.", env.getActiveProfiles()[0]);
+        log.info("감자 개수: {}", env.getProperty("gamja.count"));
+
 
     }
 }
