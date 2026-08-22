54687b871f3b44b6c462ca4500c3a7a30d2add9d
hapyjin <wimver3442@gmail.com>
2026-08-22 18:44:37 +0900

    feat(Task1): OrderEvent Publisher 추가

--- spring-fw-hyunjin/src/main/java/com/fw/week9/OrderPublisher.java
+++ spring-fw-hyunjin/src/main/java/com/fw/week9/OrderPublisher.java
@@ -1,4 +1,27 @@
 package com.fw.week9;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class OrderPublisher {
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    public OrderPublisher(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+    public void publish(OrderEvent event) {
+        event.setStatus(OrderStatus.WAIT);
+        log.info("[발행] orderId={}, name={}, count={}, status={}",
+                event.getOrderId(),
+                event.getName(),
+                event.getCount(),
+                event.getStatus());
+
+        eventPublisher.publishEvent(event);
+    }
 }
